955f9b3c4332ba51dc379bd90380f84e41a1a731
Slava <Tsvetkov@mail.ru>
2023-06-11 22:24:50 +0300

    HomeWork_1

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+//  какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first > second)
+{
+    Console.Write($"{first} большее число, а {second} меньшее число ");
+}
+else if (first < second)
+{
+    Console.Write($"{second} большее число, а {first} меньшее число ");
+}
+else
+{
+    Console.Write("числа равны");
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int third = Convert.ToInt32(Console.ReadLine());
+int max = first > second ? first : second;
+max = max > third? max : third;
+Console.Write($" максимальное число - {max} ");
