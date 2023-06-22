d754e95b954924db7436f3b199af2e69a62d7f48
Slava <Tsvetkov@mail.ru>
2023-06-22 23:01:02 +0300

    home work number 4

---
+++ Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int CountDigit(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите число, которое укладывается в диапазон от 0 до 2 147 483 647 ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    System.Console.WriteLine("вы ввели отрицательное число, попробуйте снова");
+}
+else
+{
+    System.Console.WriteLine($"Сумма всех чискл числа {number} равна {CountDigit(number)}");
+}
---
+++ Task_29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8
+//  элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]}, ");
+    }
+}
+
+System.Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+PrintArray(array);
