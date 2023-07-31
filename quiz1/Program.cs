d618f2ac0b6f7bf04fbf90ab0892762a3cde601a
Slava <Tsvetkov@mail.ru>
2023-07-31 23:35:51 +0300

    add function FillArray

--- quiz1/Program.cs
+++ quiz1/Program.cs
@@ -1,36 +1,58 @@
-﻿/*
-Задача: Написать программу, которая из имеющегося массива строк формирует
- новый массив из строк, длина которых меньше, либо равна 3 символам. 
- Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
- выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
- лучше обойтись исключительно массивами.
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+//  либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+//  пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-Примеры:
-[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-[“Russia”, “Denmark”, “Kazan”] → []
-*/
-
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
 int Promt(string message)
 {
     System.Console.WriteLine(message);
-    string result = string.Parse(Console.ReadLine()!);
-    int number = 0;
-    bool isNumber = int.TryParse(result, out number);
-    if (isNumber) return number;
-    else Console.WriteLine($"{result} - не является числом. Попробуйте заново.");
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
 }
 
 bool ValidateNumber(int value)
 {
     if (value <= 0)
     {
-        Console.WriteLine("Вы ввели число меньше или равное нулю. \n Попробуйте заново. ");
+        Console.WriteLine("Количество элементов массива не может быть меньше нуля." +
+        "\n Попробуйте заново. ");
         return false;
     }
     return true;
 }
 
+string[] FillArray(string[] array)
+{
+    int count = array.Length;
+    int i = 0;
+    while (count != 0)
+    {
+        Console.Write($"введите {i + 1}-й элемент массива   ");
+        array[i] = Console.ReadLine();
+        i++;
+        count--;
+    }
+    return array;
+}
+
+string[] ThreeSymbolArray(string[] array)
+{
+
+}
 int N = Promt("Введите число элементов массива");
 Console.WriteLine();
+string[] stringArray = new string[N];
+
+if (ValidateNumber(N))
+{
+    FillArray(stringArray);
+    Console.WriteLine($"Исходный массив - {string.Join(", ", stringArray)}");
+}
+
+
